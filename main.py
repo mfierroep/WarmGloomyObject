6c557bbe684d619c227497f00c96a3564a5d08de
mfierroep <127438202+mfierroep@users.noreply.github.com>
2023-04-18 18:50:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+d1 = random.Random()
+d2 = random.Random()
+
+rolls = []
+double = 0
+for i in range(10):
+  a = d1.randrange(1, 7)
+  b = d2.randrange(1, 7)
+  rolls.append ( (a,b) )
+  print(a, b)
+  if a + b == 12:
+    double = double + 1
+    print("DUPLO SEIS!")
+
+print(rolls)
+print(double)
